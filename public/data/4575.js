3e997182d75588eb0bb645ebaa275d6cae243c2e
Matt Dawkins <matt.dawkins@tbtmarketing.com>
2020-05-16 11:23:06 +0100

    Add actual sounds for main movements

--- public/data/4575.js
+++ public/data/4575.js
@@ -1,63 +1,65 @@
-export default {
-  movements: [
-    {
-      id: 'idle',
-      file: '',
-      startTime: 0,
-      endTime: 30,
-      loopable: false,
-      end: 'idle',
-    },
-    {
-      id: 'start',
-      file: '',
-      startTime: 30,
-      endTime: 60,
-      loopable: false,
-      end: 'cruise',
-    },
-    {
-      id: 'cruise',
-      file: '',
-      startTime: 60,
-      endTime: 90,
-      loopable: true,
-      end: 'stop',
-    },
-    {
-      id: 'stop',
-      file: '',
-      startTime: 90,
-      endTime: 120,
-      loopable: false,
-      end: 'idle',
-    },
-    {
-      id: 'move',
-      file: '',
-      startTime: 120,
-      endTime: 150,
-      loopable: false,
-      end: 'idle',
-    },
-  ],
-
-  sounds: [
-    {
-      id: 'whistle',
-      file: '',
-      startTime: 0,
-      endTime: 10,
-      loopable: false,
-      end: null,
-    },
-    {
-      id: 'blower',
-      file: '',
-      startTime: 0,
-      endTime: 20,
-      loopable: false,
-      end: null,
-    }
-  ]
-}
+export default [
+  {
+    id: 'idle',
+    file: 'data/4575.wav',
+    startTime: 138,
+    endTime: 171,
+    loopable: false,
+    end: 'idle',
+    set: 'movement',
+  },
+  {
+    id: 'start',
+    file: 'data/4575.wav',
+    startTime: 1,
+    endTime: 69,
+    loopable: false,
+    end: 'cruise',
+    set: 'movement',
+  },
+  {
+    id: 'cruise',
+    file: 'data/4575.wav',
+    startTime: 69,
+    endTime: 100,
+    loopable: true,
+    end: 'stop',
+    set: 'movement',
+  },
+  {
+    id: 'stop',
+    file: 'data/4575.wav',
+    startTime: 100,
+    endTime: 140,
+    loopable: false,
+    end: 'idle',
+    set: 'movement',
+  },
+  {
+    id: 'move',
+    file: 'data/4575.wav',
+    startTime: 173,
+    endTime: 190,
+    loopable: false,
+    end: 'idle',
+    set: 'movement',
+  },
+  {
+    id: 'whistle',
+    file: '',
+    startTime: 0,
+    endTime: 10,
+    loopable: false,
+    end: null,
+    set: 'other',
+  },
+  {
+    id: 'blower',
+    file: '',
+    startTime: 0,
+    endTime: 20,
+    loopable: false,
+    end: null,
+    set: 'other',
+  }
+]
--- src/app.js
+++ src/app.js
@@ -1,19 +1,10 @@
 import SmallPrairie from '../public/data/4575.js'
 
-// Set up controls
-SmallPrairie.movements.forEach(sound => {
-    const control = document.querySelector('.controls .control.is-hidden').cloneNode(true)
-    control.id = sound.id
-    control.querySelector('.button').innerText = sound.id
-    control.classList.remove('is-hidden')
-    if (!sound.loopable) {
-        control.querySelector('.loop').remove()
-    }
-    document.querySelector('.movements').appendChild(control)
-})
+let players = []
+let playerKeys = []
 
-// Set up sound effects
-SmallPrairie.sounds.forEach(sound => {
+// Set up controls
+SmallPrairie.forEach(sound => {
     const control = document.querySelector('.controls .control.is-hidden').cloneNode(true)
     control.id = sound.id
     control.querySelector('.button').innerText = sound.id
@@ -21,20 +12,83 @@ SmallPrairie.sounds.forEach(sound => {
     if (!sound.loopable) {
         control.querySelector('.loop').remove()
     }
-    document.querySelector('.sounds').appendChild(control)
+    players[sound.file] = new Audio(sound.file)
+    playerKeys.push(sound.file)
+    control.soundData = sound
+
+    let set = document.getElementById(sound.set)
+    if (!set) {
+        set = document.querySelector('.field.is-hidden').cloneNode(true)
+        set.classList.remove('is-hidden')
+        set.id = sound.set
+        document.querySelector('.controls').appendChild(set)
+    }
+    set.appendChild(control)
 })
+playerKeys = Array.from(new Set(playerKeys))
+
+// // Set up sound effects
+// SmallPrairie.sounds.forEach(sound => {
+//     const control = document.querySelector('.controls .control.is-hidden').cloneNode(true)
+//     control.id = sound.id
+//     control.querySelector('.button').innerText = sound.id
+//     control.classList.remove('is-hidden')
+//     audios[sound.file] = new Audio(sound.file)
+//     control.soundData = sound
+//     if (!sound.loopable) {
+//         control.querySelector('.loop').remove()
+//     }
+//     document.querySelector('.sounds').appendChild(control)
+// })
+
+// console.log(audios)
 
 // Play sounds
-document.querySelectorAll('.button').forEach(button => {
+document.querySelectorAll('.controls .button').forEach(button => {
     button.addEventListener('click', event => {
+        console.log(button.innerText)
+        if (button.innerText === 'loop') {
+            button.classList.toggle('is-info')
+            return
+        }
         // Turn off all other buttons in this set
         button.closest('.field').querySelectorAll('.button').forEach(child => {
             // Show as not active
-            child.classList.remove('is-primary')
+            child.classList.remove('is-primary', 'is-info')
             // TODO: Stop sound
+            // audios[button.parentElement.soundData.file].stop()
+            // button.closest('.control').player.pause()
         })
         // Activate this button
         button.classList.add('is-primary')
         // TODO: Start sound
+        let soundData = button.closest('.control').soundData
+        players[soundData.file].currentTime = soundData.startTime
+        players[soundData.file].play()
+        // audios[button.parentElement.soundData.file].currentTime = button.parentElement.soundData.startTime
+        // audios[button.parentElement.soundData.file].play()
     })
+})
+
+// Listen for current time
+setInterval(() => {
+    let activeButton = document.querySelector('.controls .button.is-primary')
+    if (activeButton) {
+        let activeControl = activeButton.closest('.control')
+        if (parseInt(players[activeControl.soundData.file].currentTime) === parseInt(activeControl.soundData.endTime)) {
+            if (activeControl.querySelector('.loop.is-info')) {
+                activeButton.click()
+                activeControl.querySelector('.loop').classList.add('is-info')
+                return
+            }
+            document.getElementById(activeControl.soundData.end).querySelector('.button').click()
+        }
+    }
+}, 1000)
+
+document.querySelector('.stop-all').addEventListener('click', () => {
+    playerKeys.forEach(player => {
+        players[player].pause()
+    })
+    document.querySelectorAll('.button.is-primary').forEach(button => button.classList.remove('is-primary'))
 })
