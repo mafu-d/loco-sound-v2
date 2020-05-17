9e175afc9ed5e55890c371d4a5a38b51db573046
Matt Dawkins <matt.dawkins@tbtmarketing.com>
2020-05-17 22:01:42 +0100

    Refactor to make everything more scalable

--- src/app.js
+++ src/app.js
@@ -1,101 +1,125 @@
 import SmallPrairie from './data/4575.js'
 
-let players = []
-let playerKeys = []
-
-// Set up controls
-SmallPrairie.forEach(sound => {
-    const control = document.querySelector('.controls .control.is-hidden').cloneNode(true)
-    control.id = sound.id
-    control.querySelector('.button').innerText = sound.id
-    control.classList.remove('is-hidden')
-    if (!sound.loopable) {
-        control.querySelector('.loop').remove()
+// let players = []
+// let playerKeys = []
+let sets = [
+    {
+        id: 'env',
+        name: 'Environment',
+        banks: [],
+        players: [],
+        playerKeys: [],
+    },
+    {
+        id: '4575',
+        name: '4575 Small Prairie',
+        banks: SmallPrairie,
+        players: [],
+        playerKeys: [],
     }
-    players[sound.file] = new Audio(sound.file)
-    playerKeys.push(sound.file)
-    control.soundData = sound
+]
 
-    let set = document.getElementById(sound.set)
-    if (!set) {
-        set = document.querySelector('.field.is-hidden').cloneNode(true)
-        set.classList.remove('is-hidden')
-        set.id = sound.set
-        document.querySelector('.controls').appendChild(set)
-    }
-    set.appendChild(control)
+const tabs = document.querySelector('.tabs')
+const container = document.querySelector('.container')
+
+// Create a tab for each set
+sets.forEach(set => {
+    const setElement = document.querySelector('.templates .set').cloneNode(true)
+    setElement.id = set.id
+    setElement.querySelector('h2').innerText = set.name
+    container.appendChild(setElement)
+    const setTab = document.querySelector('.templates .tab').cloneNode(true)
+    setTab.querySelector('a').innerText = set.id
+    setTab.querySelector('a').dataset.target = set.id
+    tabs.querySelector('ul').appendChild(setTab)
+
+    // Create a field for each sound bank
+    set.banks.forEach(bank => {
+        const bankElement = document.querySelector('.templates .bank').cloneNode(true)
+        bankElement.id = bank.id
+        bankElement.player = new Audio(bank.file)
+        setElement.appendChild(bankElement)
+
+        // Create a button for each sound
+        bank.sounds.forEach(sound => {
+            const soundElement = document.querySelector('.templates .sound').cloneNode(true)
+            soundElement.id = sound.id
+            soundElement.dataset.startTime = sound.startTime
+            soundElement.dataset.endTime = sound.endTime
+            if (sound.next) {
+                soundElement.dataset.next = sound.next
+            }
+            soundElement.querySelector('.button').innerText = sound.name
+            if (!sound.loopable) {
+                soundElement.querySelector('.loop').remove()
+            }
+            bankElement.appendChild(soundElement)
+        })
+    })
 })
-playerKeys = Array.from(new Set(playerKeys))
 
-// // Set up sound effects
-// SmallPrairie.sounds.forEach(sound => {
-//     const control = document.querySelector('.controls .control.is-hidden').cloneNode(true)
-//     control.id = sound.id
-//     control.querySelector('.button').innerText = sound.id
-//     control.classList.remove('is-hidden')
-//     audios[sound.file] = new Audio(sound.file)
-//     control.soundData = sound
-//     if (!sound.loopable) {
-//         control.querySelector('.loop').remove()
-//     }
-//     document.querySelector('.sounds').appendChild(control)
-// })
-
-// console.log(audios)
+// Activate tabs
+tabs.querySelectorAll('a').forEach(tab => {
+    tab.addEventListener('click', event => {
+        event.preventDefault()
+        tabs.querySelector('.is-active').classList.remove('is-active')
+        container.querySelectorAll('.set').forEach(panel => panel.classList.add('is-hidden'))
+        tab.closest('li').classList.add('is-active')
+        document.getElementById(tab.dataset.target).classList.remove('is-hidden')
+    })
+})
 
 // Play sounds
-document.querySelectorAll('.controls .button').forEach(button => {
-    button.addEventListener('click', event => {
-        console.log(button.innerText)
-        if (button.classList.contains('loop')) {
-            button.classList.toggle('is-info')
-            return
-        }
-        // Turn off all other buttons in this set
-        button.closest('.field').querySelectorAll('.button').forEach(child => {
-            // Show as not active
-            child.classList.remove('is-primary', 'is-info')
-            // TODO: Stop sound
-            // audios[button.parentElement.soundData.file].stop()
-            // button.closest('.control').player.pause()
-        })
-        // Activate this button
+document.querySelectorAll('.bank .sound .button:first-child').forEach(button => {
+    button.addEventListener('click', () => {
+        // Turn off other buttons in this bank
+        button.closest('.bank').querySelectorAll('.button').forEach(otherButton => otherButton.classList.remove('is-primary', 'is-info'))
+        // Turn on this button
         button.classList.add('is-primary')
-        // TODO: Start sound
-        let soundData = button.closest('.control').soundData
-        players[soundData.file].currentTime = soundData.startTime
-        players[soundData.file].play()
-        // audios[button.parentElement.soundData.file].currentTime = button.parentElement.soundData.startTime
-        // audios[button.parentElement.soundData.file].play()
+        console.log(button.closest('.sound').id)
+        // Play the sound
+        button.closest('.bank').player.currentTime = button.closest('.sound').dataset.startTime
+        button.closest('.bank').player.play()
     })
 })
 
-// Listen for current time
-setInterval(() => {
-    let activeButtons = document.querySelectorAll('.controls .button.is-primary')
-    if (activeButtons) {
-        activeButtons.forEach(activeButton => {
-            let activeControl = activeButton.closest('.control')
-            if (parseInt(players[activeControl.soundData.file].currentTime) === parseInt(activeControl.soundData.endTime)) {
-                if (activeControl.querySelector('.loop.is-info')) {
-                    activeButton.click()
-                    activeControl.querySelector('.loop').classList.add('is-info')
-                    return
-                }
-                if (!activeControl.soundData.end) {
-                    activeButton.classList.remove('is-primary')
-                    players[activeControl.soundData.file].pause()
-                } else {
-                    document.getElementById(activeControl.soundData.end).querySelector('.button').click()
-                }
-            }
+// Toggle loops
+document.querySelectorAll('.bank .sound .loop').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('is-info')
+    })
+})
+
+// Stop sounds
+document.querySelectorAll('.set .stop').forEach(stopButton => {
+    stopButton.addEventListener('click', () => {
+        stopButton.closest('.set').querySelectorAll('.bank').forEach(bank => {
+            bank.player.pause()
+            bank.querySelectorAll('.button').forEach(button => button.classList.remove('is-primary', 'is-info'))
         })
-    }
-}, 1000)
-
-document.querySelector('.stop-all').addEventListener('click', () => {
-    playerKeys.forEach(player => {
-        players[player].pause()
     })
-    document.querySelectorAll('.button.is-primary').forEach(button => button.classList.remove('is-primary'))
 })
+document.querySelector('.stop-all').addEventListener('click', () => {
+    document.querySelectorAll('.set .stop').forEach(stopButton => stopButton.click())
+})
+
+// Listen for next sounds
+setInterval(() => {
+    document.querySelectorAll('.button.is-primary').forEach(button => {
+        let player = button.closest('.bank').player
+        let sound = button.closest('.sound')
+        if (parseFloat(player.currentTime) >= parseFloat(sound.dataset.endTime)) {
+            button.classList.remove('is-primary')
+            if (sound.querySelector('.loop.is-info')) {
+                button.click()
+                sound.querySelector('.loop').classList.add('is-info')
+                return
+            }
+            if (typeof sound.dataset.next !== 'undefined') {
+                document.getElementById(sound.dataset.next).querySelector('.button:first-child').click()
+                return
+            }
+            player.pause()
+        }
+    })
+}, 100)
